a4f9973969432ee9e246157f2c7a189317773a4c
Mikael Paulsson <mikael.pal@gmail.com>
2020-01-03 11:44:12 +0100

    Fixed flexible versioning and docker tagging

--- build.ps1
+++ build.ps1
@@ -1,1 +1,7 @@
-docker build -t playing-with-kubernetes .
+$application="playing-with-kubernetes"
+$version=(cargo pkgid).split('#')[1]
+
+docker build -t $application':'$version .
+docker tag $application':'$version $application':latest'
+
+#docker image prune -f
